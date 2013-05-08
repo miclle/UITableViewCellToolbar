1b26d9a7bb86f8c1d0721bd0a7b89fba564e0cd6
miclle <miclle.zheng@gmail.com>
2013-05-08 17:05:00 +0800

    Add Delegate

--- UITableViewCellToolbar/ViewController.h
+++ UITableViewCellToolbar/ViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UITableViewController
+@interface ViewController : UITableViewController <UITableViewDelegate, UITableViewDataSource>
 
 @end
